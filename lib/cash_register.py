6859c12930bc3af1441e46df7f14339efd52f241
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-07-14 13:27:22 -0500

    write test solutions

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
+
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0.0
+        self.items = []
+        self.last_transaction = []
+
+    def add_item(self, name, price, count=1):
+        for i in range(count):
+            self.items.append(name)
+        self.total += price * count
+        self.last_transaction = [name, price, count]
+
+    def apply_discount(self):
+        if self.discount != 0:
+            self.total *= 1 - self.discount / 100
+            print(f"After the discount, the total comes to ${self.total:g}.")
+        else:
+            print("There is no discount to apply.")
+
+    def void_last_transaction(self):
+        self.total -= self.last_transaction[1] * self.last_transaction[2]
